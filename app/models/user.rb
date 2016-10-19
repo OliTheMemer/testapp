ac81a1e129c4e6a44fe53ef777aeb3d0aae60b4a
Oliver Cochrane <OliverCochrane125@gmail.com>
2016-10-19 15:24:08 +0100

    Added Micropost relationships and email and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
+	has_many :microposts, :dependent => :delete_all
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, presence: true
-	validates :email, presence: true, uniqueness: true
-
+	validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+	
+	has_secure_password
+	validates :password, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018145343) do
+ActiveRecord::Schema.define(version: 20161019134534) do
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -22,8 +23,9 @@ ActiveRecord::Schema.define(version: 20161018145343) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
