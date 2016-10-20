d96ac80b226fbe9e58f9f6c78bcfb886b80ad459
Oliver Cochrane <OliverCochrane125@gmail.com>
2016-10-20 14:43:02 +0100

    Users sorted alphabetically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.order(:name).paginate(page: params[:page])
   end
 
   # GET /users/1
